d869c88757d7357784a7017a43645bdcb34c9b10
potato <jacobb246@gmail.com>
2016-11-07 11:03:06 -0500

    Add existing file

--- Programs/decToBin/decToBin.c
+++ Programs/decToBin/decToBin.c
@@ -1,25 +1,24 @@
 /* Jacob Butler
-COP 1000
-proj 1 contrib 1
-description:
-decimal to binary */
+   COP 1000
+   proj 1 contrib 1
+   description:
+    decimal to binary */
 
 #include <stdlib.h>
 #include <stdio.h>
-void decToBin() {
-	int usrNm, dstNm = 0, i = 1;
-	printf("convert: "); scanf_s("%i", &usrNm);
-	while (usrNm > 0) {                           // the mathy part
-		dstNm += (usrNm % 2) * i;
-		usrNm /= 2;
-		i *= 10;
-	}
-	printf("answer: %i\n", dstNm);
-}
 
-int main() {
-	system("cls");
-	decToBin();
-	system("pause");
-	return 0;
+void decToBin() {
+  int usrNm, dstNm[500], i=0;
+  printf("convert: "); scanf("%i",&usrNm);
+  while(usrNm > 0) {
+   dstNm[i] = usrNm % 2;
+   usrNm /= 2;
+   i++;
+  }
+  i--;
+  printf("answer: ");
+  for(i;i>=0;i--){
+    printf("%i", dstNm[i]);  
+  }
+  printf("\n");
 }
